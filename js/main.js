71e3180180ad179745e4b62013e3dc66fade555a
Joris Brandt <jorisbrandt@gmail.com>
2016-10-08 03:32:19 +0200

    added access token

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+"use strict";
+
+var Botkit = require('botkit');
+
+const SLACK_TOKEN = process.env.slackkey
+const ACCESS_TOKEN = process.env.accesstoken
+
+var controller = Botkit.slackbot({
+  debug: false
+  //include "log: false" to disable logging
+  //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+  token: SLACK_TOKEN,
+}).startRTM()
+
 controller.hears(['.*'],['direct_message','direct_mention','mention', 'ambient'], function(bot,message) {
     console.log(message.text);
     if (message.type == "message") {
